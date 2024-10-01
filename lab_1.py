7d890b7bb7be83862644ed3f35713574902212be
andha8664 <anne.d.ha@sjsu.edu>
2024-10-01 01:34:29 -0700

    Added comments

--- lab_1.py
+++ lab_1.py
@@ -28,14 +28,19 @@ def extract(url):
 
 @task
 def transform(stock_1, stock_2, data1, data2):
-    results = [] # empyt list for now to hold the 90 days of stock info (open, high, low, close, volume)
-    for d in data1["Time Series (Daily)"]: # here d is a date: "YYYY-MM-DD"
+    # stock_1, stock_2: Holds symbols of stocks to add to table
+    # data1, data2: Holds json of each stock
+    
+    results = [] # empty list for now to hold the 90 days of stock info (open, high, low, close, volume)
+    for d in data1["Time Series (Daily)"]:
         stock_info = data1["Time Series (Daily)"][d]
-        stock_info['6. date'] = d
-        results.append({'0. stock': stock_1} | stock_info)
-        if len(results)>89:
+        stock_info['6. date'] = d # Append date information
+        results.append({'0. stock': stock_1} | stock_info) # Add stock symbol, "stock_1", to each data
+        if len(results)>89: # Stop adding data at 90 days
           break
-    for d in data2["Time Series (Daily)"]: # here d is a date: "YYYY-MM-DD"
+          
+    # Repeat for second stock
+    for d in data2["Time Series (Daily)"]:
         stock_info = data2["Time Series (Daily)"][d]
         stock_info['6. date'] = d
         results.append({'0. stock': stock_2} | stock_info)
@@ -45,6 +50,9 @@ def transform(stock_1, stock_2, data1, data2):
 
 @task
 def load(con, records, target_table):
+    # conn: snowflake connection
+    # records: Holds of transformed data
+    # target_table: Table used to store transformed data
     try:
         con.execute("BEGIN;")
         con.execute(f"DROP TABLE IF EXISTS {target_table};")
@@ -74,12 +82,19 @@ with DAG(
     schedule = '30 2 * * *'
 ) as dag:
     target_table = Variable.get("table")
+    
+    # Get stock urls
     url_1 = Variable.get("stock_1")
     url_2 = Variable.get("stock_2")
+    
+    # Get stock symbols
     stock_1 = Variable.get("symbol_1")
     stock_2 = Variable.get("symbol_2")
+    
+    # Initiate Snowflake connection
     cur = return_snowflake_conn()
-
+    
+    # ETL
     data1 = extract(url_1)
     data2 = extract(url_2)
     records = transform(stock_1, stock_2, data1, data2)
