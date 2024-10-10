8bbc89d8d57aa6bdc7ce5b8064433998e2df8e37
yiannnn <92844800+yiannnn@users.noreply.github.com>
2024-10-10 15:19:05 -0700

    Update train_predict.py

--- train_predict.py
+++ train_predict.py
@@ -2,6 +2,7 @@
 from airflow import DAG
 from airflow.models import Variable
 from airflow.decorators import task
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 
 from datetime import timedelta
 from datetime import datetime
